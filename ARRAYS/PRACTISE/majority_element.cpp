b66d17933ac50893ca9d71ef7940981439e35c7c
harshitgulia00 <harshitgulia599@gmail.com>
2026-07-07 15:31:30 +0530

    MAJORITY ELEMENT -- moore's voting algo || sorting algo

--- ARRAYS/PRACTISE/majority_element.cpp
+++ ARRAYS/PRACTISE/majority_element.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 int main(){
     //=============================> BRUTE-FORCE <=============================
@@ -19,7 +21,34 @@ int main(){
     // }
 
     //=============================> OPtimal One <=============================
+    // vector<int> arr = {3,3,4,2,4,4,4,4,2};
+    // int n = arr.size();
+    // int count = 0;
     
-    
+    // sort(arr.begin(), arr.end());
+    // for(int i = 0; i < n; i++){
+    //     if(arr[i] == arr[n/2]){
+    //         cout << "majority element is: " << arr[i] << endl;
+    //         break;
+    //     }
+    // }
+
+    //=============================> MOORE'S VOTING ALGORITHM {MOST OPTIMISED ALGORITHM} <=============================
+    vector<int> arr = {3,3,4,2,4,4,4,4,2};
+    int n = arr.size();
+    int count = 0;
+    int ans = 0;
+    for(int i = 0;i < n;i++){
+        if(count == 0){
+            ans = arr[i];
+        }
+        if(arr[i] == ans){
+            count++;
+        }else{
+            count--;
+        }
+    cout << count << "    ";
+    }
+    cout << "majority element is: " << ans << endl;
     return 0;
 }
